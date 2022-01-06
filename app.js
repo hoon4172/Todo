cfac1576abcb3002592dd5ed96366567c0983824
hoon <gns4172@naver.com>
2022-01-06 15:02:48 +0900

    w

--- app.js
+++ app.js
@@ -145,4 +145,5 @@ function removeLocalTodos(todo) {
     const todoIndex = todo.children[0].innerText;
     todos.splice(todos.indexOf(todoIndex),1);
     localStorage.setItem("todos",JSON.stringify(todos));
+    
 }
